bd512a46e0b4449260f11e39b98a15a9434af142
Orest Voloshchuk <orest.v@gmail.com>
2012-01-07 11:35:31 +0200

    Working on QSizePicker

---
+++ QSizePicker.cpp
@@ -0,0 +1,50 @@
+#include "QSizePicker.h"
+#include <QGridLayout>
+#include <QLabel>
+
+QSizePicker::QSizePicker(QWidget *parent) : QWidget(parent){
+    initComponents();
+}
+
+QSizePicker::QSizePicker(int maxWidth, int maxHeight, QWidget *parent) 
+:QWidget(parent) {
+    initComponents();
+    this->maxWidth = maxWidth;
+    this->maxHeight = maxHeight;
+    this->scale = (float)maxWidth/maxHeight;
+
+    m_pspWidth->setMaximum(maxWidth);
+    m_pspHeight->setMaximum(maxHeight);
+    m_pspWidth->setMinimum(MIN_WIDTH);
+    m_pspHeight->setMinimum(MIN_WIDTH/scale);
+}
+
+QSizePicker::QSizePicker(int maxWidth, float scale, QWidget *parent)
+:QWidget(parent) {
+    initComponents();
+    this->maxWidth = maxWidth;
+    this->scale = scale;
+    this->maxHeight = maxWidth/scale;
+
+    m_pspWidth->setMaximum(maxWidth);
+    m_pspHeight->setMaximum(maxHeight);
+    m_pspWidth->setMinimum(MIN_WIDTH);
+    m_pspHeight->setMinimum(MIN_WIDTH/scale);
+}
+
+void QSizePicker::initComponents() {
+    QGridLayout *pLayout = new QGridLayout();
+    m_pspWidth = new QSpinBox();
+    m_pspHeight = new QSpinBox();
+    QLabel *plblWidth = new QLabel("Width");
+    QLabel *plblHeight = new QLabel("Height");
+    m_pchkConnect = new QCheckBox("Keep A4 scale");
+
+    pLayout->addWidget(plblWidth, 0, 0);
+    pLayout->addWidget(m_pspWidth, 0, 1);
+    pLayout->addWidget(plblHeight, 1, 0);
+    pLayout->addWidget(m_pspHeight, 1, 1);
+    pLayout->addWidget(m_pchkConnect, 0, 2, 2, 1);
+
+    this->setLayout(pLayout);
+}
---
+++ QSizePicker.h
@@ -0,0 +1,30 @@
+#ifndef QSIZE_PICKER_H
+#define QSIZE_PICKER_H
+
+#include <QWidget>
+#include <QSpinBox>
+#include <QCheckBox>
+
+class QSizePicker : public QWidget {
+Q_OBJECT
+    public:
+        QSizePicker(QWidget *parent = 0);
+        QSizePicker(int maxWidth, int maxHeight, QWidget *parent = 0);
+        QSizePicker(int maxWidth, float scale, QWidget *parent = 0);
+
+        QSize value();
+    private:
+        const int MIN_WIDTH = 15;
+        void initComponents();
+        void setSpinBounds(int maxWidth, int maxHeight, float scale);
+
+        int maxWidth;
+        int maxHeight;
+        float scale;
+
+        QSpinBox *m_pspWidth;
+        QSpinBox *m_pspHeight;
+        QCheckBox *m_pchkConnect;
+};
+
+#endif
